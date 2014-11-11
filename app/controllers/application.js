dc6cb3bac8661cf1169fd15b552537bdc8de7605
Richard Livsey <richard@livsey.org>
2014-11-11 12:56:32 +0000

    update to ember 1.8.1 and add a modal

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  foo: false,
 
+  actions: {
+    launch: function() {
+      this.toggleProperty("foo");
+    }
+  }
 });
--- app/router.js
+++ app/router.js
@@ -1,13 +1,12 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-console.log(config);
-
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
+  this.modal('a-modal', {withParams: 'foo'});
 });
 
 export default Router;
